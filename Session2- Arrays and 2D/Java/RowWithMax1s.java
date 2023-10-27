71a413f8e623d22893a51121294fc1800cc653ad
Neha A <42008366+Neha-Anil@users.noreply.github.com>
2023-10-27 12:33:31 +0530

    RowWithMax1s

---
+++ Session2- Arrays and 2D/Java/RowWithMax1s.java
@@ -0,0 +1,22 @@
+// https://practice.geeksforgeeks.org/problems/row-with-max-1s0023
+
+//O(N+M)
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+        // code here
+        
+       int col = m-1;
+        int row = 0;
+        int ans = -1;
+        while(row<n && col>=0){
+            if(arr[row][col]==1){
+                ans = row;
+                col--;
+            }
+            else if(arr[row][col]==0){
+                row++;
+            }
+        }
+        return ans;
+    }
+}
