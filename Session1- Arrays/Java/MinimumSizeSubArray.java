0563ed5b63cdb67e4ace9a48ac7a146d28ea75b9
Neha A <42008366+Neha-Anil@users.noreply.github.com>
2023-11-02 10:55:30 +0530

    Update MinimumSizeSubArray.java

--- Session1- Arrays/Java/MinimumSizeSubArray.java
+++ Session1- Arrays/Java/MinimumSizeSubArray.java
@@ -1,5 +1,7 @@
 //SLIDING WINDOW PROBLEM
-
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a
+// subarray
+// whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int minLength=Integer.MAX_VALUE;
