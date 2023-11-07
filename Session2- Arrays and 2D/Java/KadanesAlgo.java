126dd96886c2ce739821607d3eeda74319a97472
Neha A <42008366+Neha-Anil@users.noreply.github.com>
2023-11-07 11:46:15 +0530

    Update KadanesAlgo.java

--- Session2- Arrays and 2D/Java/KadanesAlgo.java
+++ Session2- Arrays and 2D/Java/KadanesAlgo.java
@@ -16,6 +16,35 @@ class KadanesAlgo{
         }
         return maxSum;
     }
+    
+    //Return the subarray with max sum
+    public static int[] returnMaxSumArray(int[] array) {
+		int maxSum=Integer.MIN_VALUE;
+		int currSum=0;
+		int j=0,endIndex=0;
+		
+		for(int i=0;i<array.length;i++) {
+			currSum+=array[i];
+			
+			if(currSum>maxSum) {
+				maxSum=currSum;
+				endIndex=i;
+			}
+			if(currSum<0) {
+				currSum=0;
+				j=i+1;
+			}
+			
+			
+		}
+		int[] res=new int[endIndex-j+1];
+		int c=0;
+		for(int i=j;i<=endIndex;i++) {
+			res[c++]=array[i];
+		}
+		return res;
+		
+	}
 
     public static void main(String args[]){  
         Scanner sc = new Scanner(System.in);
